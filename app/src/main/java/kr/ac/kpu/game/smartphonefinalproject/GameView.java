7331249e52c163bb3b4584023527387a67006cdb
sungyong <tjddyddl93@gmail.com>
2020-05-17 19:30:09 +0900

    날개 추가

--- app/src/main/java/kr/ac/kpu/game/smartphonefinalproject/GameView.java
+++ app/src/main/java/kr/ac/kpu/game/smartphonefinalproject/GameView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.view.MotionEvent;
 import android.view.View;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,8 +12,15 @@ import android.graphics.Canvas;
 
 public class GameView extends View {
 
+    //캔버스
+    private int canvasWidth;
+    private int canvasHeight;
+
     //kpu로고
-    private Bitmap kpu;
+    private Bitmap kpu[] = new Bitmap[2];
+    private int KpuX=10;
+    private int KpuY;
+    private int KpuSpeed;
 
     //배경
     private Bitmap bgImage;
@@ -26,10 +34,15 @@ public class GameView extends View {
     //생명
     private Bitmap life[] = new Bitmap[2];
 
+    //상태 체크
+    private boolean touch_flg = false;
+
     public GameView(Context context) {
         super(context);
 
-        kpu = BitmapFactory.decodeResource(getResources(), R.drawable.kpu);
+        kpu[0] = BitmapFactory.decodeResource(getResources(), R.drawable.kpu1);
+        kpu[1] = BitmapFactory.decodeResource(getResources(), R.drawable.kpu2);
+
 
         bgImage = BitmapFactory.decodeResource(getResources(), R.drawable.campus);
 
@@ -46,19 +59,52 @@ public class GameView extends View {
 
         life[0] = BitmapFactory.decodeResource(getResources(), R.drawable.heart);
         life[1] = BitmapFactory.decodeResource(getResources(), R.drawable.heart_g);
+
+        //처음 위치
+        KpuY = 500;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+
         canvas.drawBitmap(bgImage, 0, 0, null);
-        canvas.drawBitmap(kpu, 0, 0, null);
+        //Kpu
+        int minKpuY = kpu[0].getHeight();
+        int maxKpuY = canvasHeight - kpu[0].getHeight() * 3;
+
+        KpuY += KpuSpeed;
+        if (KpuY < minKpuY) KpuY = minKpuY;
+        if (KpuY > maxKpuY) KpuY = maxKpuY;
+        KpuSpeed += 2;
+
+        if(touch_flg){
+            // 날개
+            canvas.drawBitmap(kpu[1], KpuX, KpuY, null);
+            touch_flg = false;
+        } else{
+            canvas.drawBitmap(kpu[0], KpuX, KpuY, null);
+        }
+
+
         canvas.drawText("Score : 0", 20, 60, scorePaint);
-        canvas.drawText("Lv.1", getWidth() /2f ,70, levelPaint);
+        canvas.drawText("Lv.1", canvasWidth /2f ,70, levelPaint);
 
         canvas.drawBitmap(life[0], 600, 30, null);
         canvas.drawBitmap(life[0], 660, 30, null);
         canvas.drawBitmap(life[1], 720, 30, null);
 
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            touch_flg = true;
+            KpuSpeed = -20;
+        }
+        return true;
     }
 }
--- app/src/main/java/kr/ac/kpu/game/smartphonefinalproject/MainActivity.java
+++ app/src/main/java/kr/ac/kpu/game/smartphonefinalproject/MainActivity.java
@@ -3,10 +3,16 @@ package kr.ac.kpu.game.smartphonefinalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private GameView gameView;
+    private Handler handler = new Handler();
+    private final static long TiMER_INTERVAL = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
         gameView = new GameView(this);
         setContentView(gameView);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+            }
+        },0, TiMER_INTERVAL);
+
+
     }
 }
